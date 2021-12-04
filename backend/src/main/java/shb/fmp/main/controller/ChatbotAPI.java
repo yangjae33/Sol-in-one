0c7a2b6681bde9da035b95b47f743f287a1a7651
hanjjm <hanjjm1994@naver.com>
2021-12-04 22:17:17 +0900

    fix: controller package 위치 변경

--- backend/src/main/java/shb/fmp/main/controller/ChatbotAPI.java
+++ backend/src/main/java/shb/fmp/main/controller/ChatbotAPI.java
@@ -1,14 +1,15 @@
-package shb.fmp.controller;
+package shb.fmp.main.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatbotAPI {
 
-    @RequestMapping(value = "/chatbot")
+    @GetMapping(value = "/chatbot")
     public String getChatting() {
-
+        System.out.println("test");
         return "test";
     }
 }
