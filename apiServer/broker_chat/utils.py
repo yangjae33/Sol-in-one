ffa6352573d7bb6cda2975076abb1d8b20d73454
mook1993 <phm_100@naver.com>
2021-12-05 17:08:31 +0900

    socket test

--- apiServer/broker_chat/utils.py
+++ apiServer/broker_chat/utils.py
@@ -1,17 +1,39 @@
-import socket
+from socket import *
+
+ip='3.37.67.26'
+port=9000
 
 def encoding_question(q_str):
-    con=socket.create_connection(('localhost',8090)) # 실제로는 model서버 ip/port값으로 넣기
+    con=socket.create_connection((ip,port)) # 실제로는 model서버 ip/port값으로 넣기
     con.send(q_str.encode())
     ret=con.recv(1024)
     return int(ret)
 
 
-def get_anser(data):
-    con=socket.create_connection(('localhost',8090)) # 실제로는 model서버 ip/port값으로 넣기
-    con.send(data.encode())
-    ret=con.recv(1024)
+# def get_answer(data):
+#     print(data)
+#     con=socket.create_connection((ip,port)) # 실제로는 model서버 ip/port값으로 넣기
+#     print(con)
+#     con.send(data.encode())
+#     print(data.encode(),'!!!')
+#     ret=con.recv(1024)
+#     print(ret)
+#     return ret.split('|')
+
+
+def get_answer(data):
+    print(data)
+    clientSock = socket(AF_INET, SOCK_STREAM)
+    clientSock.connect((ip, port))
+    print(clientSock)
+    clientSock.send(data.encode('utf-8'))
+    print(data.encode('utf-8'),'!!!')
+    ret=clientSock.recv(1024)
+    print(ret)
     return ret.split('|')
+
+
+#print(get_answer("/message/?{\'data\':\'질문\'}"))
 
 
 # ip='localhost'
